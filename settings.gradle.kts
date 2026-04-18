b1d02b6c74f66bfafd543463f925051680ea7f6f
Chen Meng <chenmeng466@outlook.com>
2026-04-18 22:07:35 +0800

    删除 Aliyun maven repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,5 @@
 pluginManagement {
     repositories {
-        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
         gradlePluginPortal()
         mavenCentral()
         exclusiveContent {
